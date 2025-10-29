39deedf11cdb8729096a407f8f5d182c262e3ac6
Haleyunblockedgames <157207121+haleyschoolubg77@users.noreply.github.com>
2025-10-29 10:27:58 +0700

    3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-// Google Analytics
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-P4EX4ZRLS2");
+// Optimized Google Analytics
+(function() {
+  var ga_id = 'G-P4EX4ZRLS2';
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + ga_id;
+  var firstScript = document.getElementsByTagName('script')[0];
+  firstScript.parentNode.insertBefore(script, firstScript);
+  
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', ga_id, {
+    'page_path': window.location.pathname,
+    'transport_type': 'beacon'
+  });
+})();
